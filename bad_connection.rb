d4eb10cf4a573f6ffb3e0e0ecd11eb3601e232c2
ccorrigan <barnumswinter@gmail.com>
2017-03-16 10:31:29 -0600

    Fixed indentation spacing from 4 spaces to 2

--- bad_connection.rb
+++ bad_connection.rb
@@ -6,16 +6,16 @@ puts "HELLO, THIS IS A GROCERY STORE!"
 until ready_to_quit do
   input = gets.chomp 
     if input.empty?
-        puts "HELLO?!"
+      puts "HELLO?!"
     elsif input == input.downcase
-        puts "I AM HAVING A HARD TIME HEARING YOU."
+      puts "I AM HAVING A HARD TIME HEARING YOU."
     elsif input == "GOODBYE!" && bye == 0
-        bye +=1
-        puts "ANYTHING ELSE I CAN HELP WITH?"
+      bye +=1
+      puts "ANYTHING ELSE I CAN HELP WITH?"
     elsif input == "GOODBYE!" && bye > 0
-        ready_to_quit = true
+      ready_to_quit = true
     else  
-        puts "NO, THIS IS NOT A PET STORE"
+      puts "NO, THIS IS NOT A PET STORE"
     end
 end
 
