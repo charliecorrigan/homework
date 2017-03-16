1a4f65d250edcdd3ce938d5e9a06d16b4a4254e6
ccorrigan <barnumswinter@gmail.com>
2017-03-16 04:52:44 -0600

    pulled out 'dividible_by?' methods

--- super_fizz.rb
+++ super_fizz.rb
@@ -1,8 +1,23 @@
-(0..1000).each do |i|
+def divisible_by_7?(i)
+    i % 7 == 0
+end
+
+def divisible_by_3?(i)
+    i % 3 == 0
+end
+
+def divisible_by_5?(i)
+    i % 5 == 0
+end
+
+range_min = 0
+range_max = 1000
+
+(range_min..range_max).each do |i|
     message = ""
-    if i % 7 == 0 then message += "Super" end
-    if i % 3 == 0 then message += "Fizz" end
-    if i % 5 == 0 then message += "Buzz" end
+    if divisible_by_7?(i) then message += "Super" end
+    if divisible_by_3?(i) then message += "Fizz" end
+    if divisible_by_5?(i) then message += "Buzz" end
     if message.empty? then message = i end
     puts message
 end
