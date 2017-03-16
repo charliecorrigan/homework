48450ca2d24f98960cdb289d9e0cd0580d5bc0e7
ccorrigan <barnumswinter@gmail.com>
2017-03-16 10:28:33 -0600

    Fixed indentation from 4 spaces to 2

--- super_fizz.rb
+++ super_fizz.rb
@@ -1,23 +1,23 @@
 def divisible_by_7?(i)
-    i % 7 == 0
+  i % 7 == 0
 end
 
 def divisible_by_3?(i)
-    i % 3 == 0
+  i % 3 == 0
 end
 
 def divisible_by_5?(i)
-    i % 5 == 0
+  i % 5 == 0
 end
 
 range_min = 0
 range_max = 1000
 
 (range_min..range_max).each do |i|
-    message = ""
-    if divisible_by_7?(i) then message += "Super" end
-    if divisible_by_3?(i) then message += "Fizz" end
-    if divisible_by_5?(i) then message += "Buzz" end
-    if message.empty? then message = i end
-    puts message
+  message = ""
+  if divisible_by_7?(i) then message += "Super" end
+  if divisible_by_3?(i) then message += "Fizz" end
+  if divisible_by_5?(i) then message += "Buzz" end
+  if message.empty? then message = i end
+  puts message
 end
